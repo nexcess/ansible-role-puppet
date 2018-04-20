c89227e0c887babae9d34203ef25d4482015f08f
Chris Orlando <chrsorlando@gmail.com>
2018-04-20 16:46:23 -0400

    Allow setting the puppet environment to use during puppet run

--- files/launch-puppet.sh
+++ files/launch-puppet.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-/opt/puppetlabs/bin/puppet agent -t --logdest /root/puppet.log
+/opt/puppetlabs/bin/puppet agent -t --logdest /root/puppet.log --environment "$1"
 
 if [[ "$?" != 2 ]]; then
-  /opt/puppetlabs/bin/puppet agent -t --logdest /root/puppet.log
+  /opt/puppetlabs/bin/puppet agent -t --logdest /root/puppet.log --environment "$1"
 fi 
 
