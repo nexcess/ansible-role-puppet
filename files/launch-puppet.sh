723d13ee51428f81042f3b0d783731844f1fb51c
Chris Orlando <chrsorlando@gmail.com>
2018-06-20 09:49:47 -0400

    [NSO-2210] Kill puppet before each puppet run

--- files/launch-puppet.sh
+++ files/launch-puppet.sh
@@ -3,6 +3,7 @@
 /opt/puppetlabs/bin/puppet agent -t --logdest /root/puppet.log
 
 if [[ "$?" != 2 ]]; then
+  pkill -f /bin/puppet
   /opt/puppetlabs/bin/puppet agent -t --logdest /root/puppet.log
 fi 
 
